8a8d908b5379f0173cf8e3a9a36d48ed5cf42f10
Kartikgauniyal <146377144+Kartikgauniyal@users.noreply.github.com>
2024-02-18 20:18:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	let inps = document.querySelectorAll("input")
+let first_name = inps[0].value 
+	let last_name = inps[1].value
+	let fullname = first_name+" "+ last_name
+	alert(fullname)
 }
